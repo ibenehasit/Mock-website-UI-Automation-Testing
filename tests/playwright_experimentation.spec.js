527d4e3aa03c279daba4e6e58190a09d915b6c5e
ibenehasit <ibeneessem@hotmail.com>
2026-07-06 17:04:19 +0100

    Added an Invalid Login Test

--- tests/playwright_experimentation.spec.js
+++ tests/playwright_experimentation.spec.js
@@ -1,7 +1,24 @@
 import{test, expect} from '@playwright/test'
 
-test('Playwright Experimentation', async({page}) => {
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+
+});
+
+test('Correct Website Title', async({page}) => {
     await page.pause()
-	await page.goto('http://localhost:8080/')
 	await expect(page).toHaveTitle("Run Marketplace - Nigeria's Trusted Shopping Platform")
+})
+
+
+
+test('Invalid Login Test', async({page}) => {
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).fill('username@email.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('password123');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.pause()
+    await expect(page.getByRole('alert')).toBeVisible();
 })
